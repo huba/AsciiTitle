08c31cf3fe1cbea0419f42693189b3d566d4290a
Huba Nagy <12huba@gmail.com>
2012-11-20 21:53:24 +1300

    Removed silly mistake...

--- lib/ascii_title.rb
+++ lib/ascii_title.rb
@@ -1,8 +1,8 @@
-require "lib_ascii_title/version"
+require "ascii_title/version"
 
-require "lib_ascii_title/string_extension"
-require "lib_ascii_title/font_face"
-require "lib_ascii_title/utils"
+require "ascii_title/string_extension"
+require "ascii_title/font_face"
+require "ascii_title/utils"
 
 module AsciiTitle
 	
